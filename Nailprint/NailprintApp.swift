c4b2bef84d6550b819ea1b98f77a6f6b08ec7a14
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2023-03-12 13:13:22 +0300

    Initial Commit

---
+++ Nailprint/NailprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  NailprintApp.swift
+//  Nailprint
+//
+//  Created by Mesut Yılmaz on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NailprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
